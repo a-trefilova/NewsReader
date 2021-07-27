1e08c7045b885515853020af29eeb29153ee32b6
a-trefilova <atrefilova97@icloud.com>
2021-07-27 10:34:34 +0300

    Initial commit

---
+++ NewsReader/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  NewsReader
+//
+//  Created by 19399090 on 27.07.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+}
+

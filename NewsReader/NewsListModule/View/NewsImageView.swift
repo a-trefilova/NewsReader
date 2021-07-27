376e1612c2ecca2edf34363913b53cdac6da4dac
a-trefilova <atrefilova97@icloud.com>
2021-07-27 16:27:43 +0300

    Table view created and data passed

---
+++ NewsReader/NewsListModule/View/NewsImageView.swift
@@ -0,0 +1,38 @@
+
+import UIKit
+
+class NewsImageView: UIImageView {
+    
+    private let placeholderImage = UIImage(named: "")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func downloadImage(from urlString: String?) {
+        guard let string = urlString,
+              let url = URL(string: string) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let self = self,
+                  error == nil,
+                  let response = response as? HTTPURLResponse,
+                  response.statusCode == 200,
+                  let data = data,
+                  let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async { self.image = image }
+        }
+        task.resume()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
--- NewsReader/NewsListModule/View/NewsListVC.swift
+++ NewsReader/NewsListModule/View/NewsListVC.swift
@@ -8,24 +8,67 @@ protocol NewsListViewProtocol: AnyObject {
 
 class NewsListVC: UIViewController {
     
+    private var tableView = UITableView(frame: .zero, style: .grouped)
+    private var newsItemsList: [NewsItem] = []
     var presenter: NewsListPresenterProtocol!
   
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.configureView()
         presenter.showListOfItems()
+        setupTableView()
+        layoutTableView()
     }
 
+    private func setupTableView() {
+        view.backgroundColor = .systemBackground
+        tableView.register(NewsItemCell.self, forCellReuseIdentifier: NewsItemCell.reuseId)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func layoutTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
 
 extension NewsListVC: NewsListViewProtocol {
     
     func prepareView() {
-        view.backgroundColor = .red
+
     }
     
     func showListOfItems(items: [NewsItem]) {
-        print("Showing empty items")
+        newsItemsList = items
+        tableView.reloadData()
     }
     
+}
+
+extension NewsListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        newsItemsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsItemCell.reuseId, for: indexPath) as? NewsItemCell else { return UITableViewCell() }
+        let currentItem = newsItemsList[indexPath.row]
+        cell.configure(with: currentItem)
+        return cell
+    }
+    
+    
+}
+
+extension NewsListVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
 }
